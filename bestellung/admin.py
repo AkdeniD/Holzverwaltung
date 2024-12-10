8c42287ff3e2ba790814ada7b1e8ee1384bb8269
akdenizd <dennis.akdeniz@teilnehmer-bfwsb.de>
2024-12-10 16:09:36 +0100

    admin.register Lagerplatz hinzugefügt

--- bestellung/admin.py
+++ bestellung/admin.py
@@ -6,9 +6,14 @@ admin.site.register(Lieferant)
 admin.site.register(Lieferung)
 #admin.site.register(Artikel)
 admin.site.register(Lieferposition)
-admin.site.register(Lagerplatz)
+#admin.site.register(Lagerplatz)
 
 @admin.register(Artikel)
 class ArtikelAdmin(admin.ModelAdmin):
     search_fields = ['bezeichnung', 'bemerkung']
     list_display = ['bezeichnung', 'liefereinheit', 'ekpreis', 'bemerkung']
+
+
+@admin.register(Lagerplatz)
+class LagerplatzAdmin(admin.ModelAdmin):
+    list_filter = ['artikel']
